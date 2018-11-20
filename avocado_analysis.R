236a70daacbf70f53e85797d86d5b99b94475572
Robin <ra.choudhury@ufl.edu>
2018-11-20 10:19:08 -0500

    data, code, figures

---
+++ avocado_analysis.R
@@ -0,0 +1,95 @@
+library(tidyverse)
+library(vegan)
+
+data=read_csv("data/avocado_rating.csv") %>%
+  mutate(variety = tools::toTitleCase(variety),
+         rater = tools::toTitleCase(rater))
+
+ggplot(data, aes(reorder(variety, rating, FUN = mean ), rating)) +
+  geom_point(aes(color = rater), size = 2) +
+  geom_boxplot(alpha=0.3) +
+  scale_y_continuous(limits = c(1,10), breaks = seq(2,10,2))+
+  ylab("Rating (1-10)") + 
+  xlab("Avocado Variety")  +
+  theme_classic() +
+  theme(legend.position = "bottom",
+        text = element_text(size = 20),
+        panel.background = element_rect(fill = "transparent",colour = NA),
+        plot.background = element_rect(fill = "transparent",colour = NA),
+        legend.box.background = element_rect(fill = "transparent",colour = NA))
+
+ggsave(filename = paste(Sys.Date(),".variety.rating.png", sep = ""), 
+       width = 10, 
+       height = 6, 
+       units = "in",
+       dpi = 300, 
+       type="cairo-png", 
+       bg="transparent")
+
+data_summary <- data %>%
+  group_by(rater, variety) %>%
+  summarize(mean=mean(rating))
+
+ggplot(data = data_summary, aes(x = reorder(rater, mean, FUN = median), y = mean, group = variety)) +
+  geom_line(aes(color = variety), size = 2) +
+  geom_point(aes(color = variety), size = 4)+
+  xlab("Person")+
+  ylab("Rating")+
+  theme_classic() +
+  theme(legend.position = "bottom",
+        text = element_text(size = 20),
+        panel.background = element_rect(fill = "transparent",colour = NA),
+        plot.background = element_rect(fill = "transparent",colour = NA),
+        legend.box.background = element_rect(fill = "transparent",colour = NA))
+
+
+ggsave(filename = paste(Sys.Date(),".variety.rating.person.png", sep = ""), 
+       width = 10, 
+       height = 6, 
+       units = "in",
+       dpi = 300, 
+       type="cairo-png", 
+       bg="transparent")
+#ordination?
+
+rater_spread <- data_summary %>%
+  spread(rater, mean) %>%
+  column_to_rownames(., "variety") %>%
+  as.matrix(.)
+rater_spread[is.na(rater_spread)] <- 0
+
+ord<- decorana(veg = rater_spread )
+
+plot(ord)
+
+rater_mean <- data %>%
+  group_by(rater) %>%
+  summarise(mean = mean(rating))
+
+ord_data = ord$cproj %>%
+  as.data.frame(.) %>%
+  rownames_to_column(., "rater") %>%
+  left_join(., rater_mean)
+
+ggplot(ord_data, aes(DCA1, DCA2, label=rater))+
+  geom_text(aes(color=mean), fontface = "bold", size=6) +
+  theme_classic() +
+  scale_color_viridis_c(option = "D",
+                        guide = guide_colourbar(barwidth = 20, 
+                                                barheight = 2,
+                                                title.position = "top",
+                                                title.hjust = 0.5,
+                                                title = "Mean Rating"))+
+  theme(legend.position = "bottom",
+        text = element_text(size=20),
+        panel.background = element_rect(fill = "transparent",colour = NA),
+        plot.background = element_rect(fill = "transparent",colour = NA),
+        legend.box.background = element_rect(fill = "transparent",colour = NA))
+
+ggsave(filename = paste(Sys.Date(),".variety.ordination.png", sep = ""), 
+       width = 10, 
+       height = 6, 
+       units = "in",
+       dpi = 300, 
+       type="cairo-png", 
+       bg="transparent")
